a43cbce4954f7841e29cacc96907bea948a7c94e
public202002 <public202002@gmail.com>
2021-07-13 13:35:13 +0200

    working vizonejs

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ window.app = angular.module('app', requires);
 
 // see also: https://github.com/ded/script.js
 import f from "./test/testik.js"
+
 console.log(f())
 
 angular.bootstrap(document.getElementById('app'), ['app']);
 
-// import "~/test/vizone.js"
